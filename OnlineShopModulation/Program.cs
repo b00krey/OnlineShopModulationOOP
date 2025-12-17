c898069f4bccae743ef10231576cb0e387db191b
b00krey <tacticooler@yandex.ru>
2025-12-17 22:06:59 +0300

    Basket added

--- OnlineShopModulation/Program.cs
+++ OnlineShopModulation/Program.cs
@@ -1,7 +1,12 @@
 ﻿namespace OnlineShopModulation
 {
-    internal class Program
+    static class Program
     {
+        static bool IsYes(string answer)
+        {
+            return answer.ToLower() == "да";
+        }
+
         static void Main(string[] args)
         {
             Store onlineStore = new Store();
@@ -19,6 +24,20 @@
                 default:
                     Console.WriteLine("Выберите номер действия из списка");
                     break;
+            }
+
+            Console.WriteLine("Хотите добавить продукт в корзину? Наберите Да или Нет.");
+            bool yes = IsYes(Console.ReadLine());
+
+            while (IsYes)
+            {
+                onlineStore.ShowCatalog();
+                Console.WriteLine("Напишите номер продукта, которого нужно добавить в корзину");
+                int numberProduct = int.Parse(Console.ReadLine());
+                onlineStore.AddToBasket(numberProduct);
+
+                Console.WriteLine("Хотите добавить продукт в корзину? Наберите Да или Нет.");
+                yes = IsYes(Console.ReadLine());
             }
         }
     }
--- OnlineShopModulation/Store.cs
+++ OnlineShopModulation/Store.cs
@@ -3,7 +3,7 @@
     public class Store
     {
         public List<Product> Products;
-
+        public List<Product> Basket;
         public Store()
         {
             Products = new List<Product>
@@ -15,12 +15,26 @@
             new Product("Йогурт", 300),
             new Product("Сок", 80)
             };
+
+            Basket = new List<Product>();
         }
 
         public void ShowCatalog()
         {
+            int number = 1;
             foreach (Product product in Products)
+            {
+                Console.Write(number + ". ");
                 product.Print();
+                number++;
+            }
+        }
+
+       public void AddToBasket(int numberProduct)
+        {
+            Basket.Add(Products[numberProduct - 1]);
+            Console.WriteLine($"Продукт {Products[numberProduct - 1].Name} успешно добавлен в корзину.");
+            Console.WriteLine($"В корзине {Basket.Count} продуктов.");
         }
     }
 }
